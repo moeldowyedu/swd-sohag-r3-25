a14ea80565957eb43bfa153fef1dcb2f63ca2bee
Mohammed Salah <Mofree88@outlook.com>
2025-06-24 13:43:05 +0300

    Day3 is done

--- dashboard.php
+++ dashboard.php
@@ -4,19 +4,20 @@ if($_SERVER['REQUEST_METHOD']=='POST' && isset($_FILES['file'])){
     $name = $_POST['name'];
     $fileName = $_FILES['file']['name'];
     // array of allowed image extensions
-    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+/*    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
     $filenameParts = explode('.', $fileName);//returns an indexed array of file name parts [0 => 'example', 1 => 'extension']
-    $fileExtension = strtolower(end($filenameParts)); // get the last part of the array, which is the file extension
+    $fileExtension = strtolower(end($filenameParts)); // get the last part of the array, which is the file extension*/
     $fileType = $_FILES['file']['type'];
     // array of allowed file types for images only
     $allowedFileTypes = ['image/jpeg', 'image/png', 'image/gif'];
     $fileTempName = $_FILES['file']['tmp_name'];
     $fileSize = $_FILES['file']['size'];
-    if(in_array($fileExtension,$allowedExtensions)){
+    if(in_array($fileType,$allowedFileTypes)&& $fileSize < 5000000){
+        // check if the file type is allowed and file size is less than 5MB
         move_uploaded_file($fileTempName,"images/".$fileName);
         echo "File uploaded successfully: " . htmlspecialchars($fileName) . "<br>";
     }else{
-        echo "Invalid file type. Only JPG, JPEG, PNG, and GIF files are allowed.<br>";
+        echo "Invalid file type. Only JPG, JPEG, PNG, and GIF files are allowed. OR file size is bigger than 5 MB<br>";
     }
 }else {
     echo "No file uploaded or invalid request.";
